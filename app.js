3fe845712da7c87a866cc431b6ded9b2f8825ec4
Taufiq Abdullah <taufiqtab77@gmail.com>
2021-05-19 14:35:57 +0700

    Express Training

---
+++ app.js
@@ -0,0 +1,53 @@
+const e = require('express')
+const express = require('express')
+var mysql = require('mysql')
+const app = express()
+const port = 3000
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'expresso',
+    multipleStatements : true
+  })
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/coffee', (req, res) => {
+    let coffee;
+    connection.query('SELECT * from coffee', function (err, result, fields) {
+        if (err){
+            console.log(err);//throw err  
+        } else {
+            coffee = result
+        }
+        res.json({data : coffee})
+    })
+  })
+
+  app.get('/coffeecount', (req, res) => {
+    let data;
+    let total = 0;
+    let totalQty = 0;
+    
+    connection.query('SELECT * from coffee; select count(*) as total from coffee;select SUM(qty) as totalQty from coffee',function(err, result){
+        if(err){
+            throw err;
+        }else{
+            data = result[0];
+            total = result[1][0].total;
+            totalQty = result[2][0].totalQty
+            res.json({data, total, totalQty})
+        }
+    });
+    
+  })
+
+app.listen(port, () => {
+  console.log(`Your Coffee ready at http://localhost:${port}`)
+})
