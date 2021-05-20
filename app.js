50ef93ea22f07d2ae664c7984830d14257cb150b
Taufiq Abdullah <taufiqtab77@gmail.com>
2021-05-20 11:30:42 +0700

    cert

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const e = require('express')
 const express = require('express')
 var mysql = require('mysql')
 const second = require('./second.js');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
 const app = express()
 const port = 3000
 
@@ -13,7 +15,11 @@ var connection = mysql.createConnection({
     multipleStatements : true
   })
 
-
+  app.get('/jwt', (req, res) => {
+    let privateKey = fs.readFileSync('./private.pem', 'utf8');
+    let token = jwt.sign({ "body": "stuff" }, privateKey, { algorithm: 'HS256'});
+    res.send(token);
+})
 
 app.get('/', (req, res) => {
   res.send('Assalamualaikum Bunda!')
@@ -49,6 +55,38 @@ app.get('/coffee', (req, res) => {
     });
     
   })
+
+  app.get('/secret', isAuthorized, (req, res) => {
+    res.json({ "message" : "THIS IS SUPER SECRET, DO NOT SHARE!" })
+})
+
+function isAuthorized(req, res, next) {
+  if (typeof req.headers.authorization !== "undefined") {
+      // retrieve the authorization header and parse out the
+      // JWT using the split function
+      let token = req.headers.authorization.split(" ")[1];
+      
+      let privateKey = fs.readFileSync('./private.pem', 'utf8');
+      // Here we validate that the JSON Web Token is valid and has been 
+      // created using the same private pass phrase
+      jwt.verify(token, privateKey, { algorithm: "HS256" }, (err, user) => {
+          
+          // if there has been an error...
+          if (err) {  
+              // shut them out!
+              res.status(500).json({ error: "Not Authorized" });
+          }
+          // if the JWT is valid, allow them to hit
+          // the intended endpoint
+          return next();
+      });
+  } else {
+      // No authorization header exists on the incoming
+      // request, return not authorized
+      res.status(500).json({ error: "Not Authorized" });
+  }
+}
+
 
 app.listen(port, () => {
   console.log(`Your Coffee ready at http://localhost:${port}`)
